3dbb7b75f87083d99366c2458fa3408babebb21f
MINKOVICT <167470199+MINKOVICT@users.noreply.github.com>
2024-04-24 11:47:29 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom="MINKO"
+age=25
+taille=1.85
+est_étudiant="true"
+print(f"Bonjour , je suis {nom} j'ai {age} ans et je mesure {taille} m. êtes vous étudiant ? {est_étudiant} !")
